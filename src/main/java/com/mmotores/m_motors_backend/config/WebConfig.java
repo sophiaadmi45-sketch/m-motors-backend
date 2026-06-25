1654ce7997b132c0994427a41f79a19782bf21e8
Sophia Ouggahi <sophia.admi45@gmail.com>
2026-06-25 14:05:14 +0200

    fix: ajout du WebConfig pour servir les images du cloud

---
+++ src/main/java/com/mmotores/m_motors_backend/config/WebConfig.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    
+        String dossierTemporaire = System.getProperty("java.io.tmpdir") + "/images/";
+        
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:" + dossierTemporaire);
+    }
+}
